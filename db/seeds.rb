be02e8fa09ebf5e0e3212031a7cf7a8329d832c5
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-06-01 00:11:51 +0900

    updated style

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ sticky_img = URI.open("https://res.cloudinary.com/dzwpbkn3u/image/upload/v162218
 sticky = Prank.create!(name: "Cover something in sticky notes", content: "I will cover the requested item in sticky notes. Disclaimer: I won't do anything illegal (break into someones house etc.)", price: 5000, city:'Ichikawa', user: User.all.sample)
 sticky.photo.attach(io: sticky_img, filename: 'prank.png', content_type: 'image/png')
 
-bucket_img = URI.open("https://res.cloudinary.com/dzwpbkn3u/image/upload/v1622099575/dribbble_xcsz2o.png")
+bucket_img = URI.open("https://res.cloudinary.com/dzwpbkn3u/image/upload/v1622472060/image_cpqmbu.png")
 bucket = Prank.create!(name: "Install water bucket prank", content: "I will professionally install a waterbucket in the place of your choosing. Most common places include: Bedroom door and Bathroom door.", price: 2000, city:'Yokohama', user: User.all.sample)
 bucket.photo.attach(io: bucket_img, filename: 'bucket.png', content_type: 'imag/png')
 
